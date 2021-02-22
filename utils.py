f01c597b7271407a568c23b9469181dae154b148
Yaosheng Xu <xujohnathan021@163.com>
2021-02-21 22:50:33 -0800

    finished tensorboardlog in utils, add main_with_logging.py

--- utils.py
+++ utils.py
@@ -1,2 +1,18 @@
 # tensorboard
 from torch.utils.tensorboard import SummaryWriter
+
+from datetime import datetime
+
+
+class Tensorboardlog:
+
+    now = str(datetime.now().strftime(r"%d%m%Y_%H_%M"))
+    writer = SummaryWriter(f"runs/{now}")
+
+    @classmethod
+    def tensorboardlog(cls, episode_num, average_reward):
+        """
+        episode_num: current episode number
+        average_reward: average reward until current episode
+        """
+        cls.writer.add_scalar("episode_reward_mean", average_reward, episode_num)
